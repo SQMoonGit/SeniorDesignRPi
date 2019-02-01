2c5aa8d45c3af6cf949b6105c3b5c0f9f5e1821f
ShockingIce <hykmoon@gmail.com>
2019-02-01 11:47:11 -0600

    Python version of SerialConnections

---
+++ SeniorDesignRPi/src/SerialConnections.py
@@ -0,0 +1,20 @@
+class SerialConnections:
+  sensor = False
+  arduinos = False
+  power = False
+  gnd = False
+  internet = False
+  GCP = False
+  
+  
+  def checkSensor():
+  
+  def checkArduinos():
+  
+  def checkPower():
+  
+  def checkGND():
+  
+  def checkInternet():
+  
+  def checkGCP():
