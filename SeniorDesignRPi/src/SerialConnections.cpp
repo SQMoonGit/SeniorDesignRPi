36abcb11efe9d9696fd1a9ec2768e0d882bb3275
ShockingIce <hykmoon@gmail.com>
2019-02-07 20:48:19 -0600

    Update (#1)

--- SeniorDesignRPi/src/SerialConnections.cpp
+++ SeniorDesignRPi/src/SerialConnections.cpp
@@ -7,9 +7,47 @@
 
 #include <iostream>
 #include <string>
+#includ "SerialConnection.h"
+
 
 SerialConnections::SerialConnections()
 {
 
 
 }
+
+SerialConnections:: ~SerialConnections()
+{
+
+}
+
+SerialConnections:: void checkSensor()
+{
+
+}
+
+SerialConnections:: void checkArduinos()
+{
+
+}
+
+SerialConnections:: void checkPower()
+{
+
+}
+
+SerialConnections:: void checkGND()
+{
+
+}
+
+SerialConnections:: void checkInternet()
+{
+
+}
+
+SerialConnections:: void checkGCP()
+{
+
+}
+
