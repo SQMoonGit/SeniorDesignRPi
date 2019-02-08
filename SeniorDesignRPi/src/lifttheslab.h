6938cc69e2c1aac89c3b2385334a74599c8de4ac
ShockingIce <hykmoon@gmail.com>
2019-02-07 20:37:51 -0600

    Possible variables and functions

--- SeniorDesignRPi/src/lifttheslab.h
+++ SeniorDesignRPi/src/lifttheslab.h
@@ -2,10 +2,17 @@
 
 class LiftTheSlab
 {
-
-
+private:
+  
+  double FinalHeight;                   //Final height variable
+  double calculateFinalHeight();        //Calculate the final height
+  boolean valveSwitch();                //Switch the valve on and off
+                          
 public:
-  LiftTheSlab();
-  ~LiftTheSlab();
-
+  LiftTheSlab();                        //default constructor
+  ~LiftTheSlab();                       //deconstructor
+  double sensorHeight();                //Get the sensor height
+  double getFinalHeight();              //Get the final height
+  boolean getValveValue();              //Get the value of the valve
+  void lift();                          //main algorithm for lifting the slab
 }
